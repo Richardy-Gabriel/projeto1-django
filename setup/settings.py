e07befabfb598dc5e446fa0fba8749bcd2be3468
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2024-03-13 15:38:51 -0300

    Register models in the admin, configure local MEDIA and configure urls.

--- setup/settings.py
+++ setup/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
 # Coleta todos os arquivos staticos e coloca em apenas uma pasta, config para produção
 STATIC_ROOT = BASE_DIR / 'static'
 
+# Coleta todas as imagens que serão inseridas no DB
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
