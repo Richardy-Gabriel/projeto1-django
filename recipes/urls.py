20dd6f56d3ed553c72282da60960f48f56c9448a
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2024-03-13 10:29:36 -0300

    Create name space in urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 # Para segurança na aplicação é recomendado adicionar o tipo
 # que devemos receber como parametro: str, slug, int
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
